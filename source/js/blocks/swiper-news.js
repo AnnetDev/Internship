a2f07704847a80a7f9c00f94b7c58fda0ef6d003
Anna <annabaidikova92@gmail.com>
2025-02-13 11:27:21 +0100

    news-swiper developing

---
+++ source/js/blocks/swiper-news.js
@@ -0,0 +1,46 @@
+import Swiper from 'swiper';
+import { Navigation, Pagination, Grid } from 'swiper/modules';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/pagination';
+import 'swiper/scss/grid';
+
+export function initializeNewsSwiper() {
+  const newsSwiper = new Swiper('.news__swiper', {
+    modules: [Navigation, Pagination, Grid],
+    direction: 'horizontal',
+    // spaceBetween: 20,
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    grid: {
+      rows: 2,
+      fill: 'column',
+    },
+    pagination: {
+      el: '.news__swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.news__swiper-button.swiper-button-next',
+      prevEl: '.news__swiper-button.swiper-button-prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 2,
+          fill: 'row',
+        },
+      },
+      1440: {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+          fill: 'row',
+        },
+      },
+    },
+  });
+}
