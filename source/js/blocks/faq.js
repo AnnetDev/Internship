eb175cfbc6b76a84ba23c4e980df516bd41863b0
Anna <annabaidikova92@gmail.com>
2025-02-19 22:26:18 +0100

    faq: tab navigation fixed

--- source/js/blocks/faq.js
+++ source/js/blocks/faq.js
@@ -1,20 +1,41 @@
 export const toggleAccordion = () => {
   document.addEventListener('DOMContentLoaded', () => {
     const listElements = document.querySelectorAll('.faq__list-item');
+    const buttons = document.querySelectorAll('.faq__button');
+    buttons.forEach((button) => {
+      button.setAttribute('tabindex', '-1');
+    });
+
+
     listElements.forEach((listElement) => {
       const question = listElement.querySelector('.faq__header'); // Select the header of each item
       const questionButton = listElement.querySelector('.faq__button'); // Select the button of each item
 
+      const toggleItem = () => {
+        listElement.classList.toggle('faq__list-item--active');
+        questionButton.classList.toggle('faq__button--active');
+        const isExpanded = questionButton.classList.contains('faq__button--active');
+        questionButton.setAttribute('aria-expanded', isExpanded);
+      };
+
+      // Event listener for mouse click on the header
       question.addEventListener('click', (event) => {
         event.stopPropagation();
-        listElement.classList.toggle('faq__list-item--active');
-        questionButton.classList.toggle('faq__button--active');
+        toggleItem();
       });
 
+      // Event listener for mouse click on the button
       questionButton.addEventListener('click', (event) => {
         event.stopPropagation();
-        listElement.classList.toggle('faq__list-item--active');
-        questionButton.classList.toggle('faq__button--active');
+        toggleItem();
+      });
+
+      // Event listener for keyboard interaction (Enter key)
+      question.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+          event.preventDefault();
+          toggleItem();
+        }
       });
     });
   });
